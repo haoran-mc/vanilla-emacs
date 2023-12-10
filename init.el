634ac482bc27a6a06a029b8f529f44adf72e85bc
haoran <haoran.mc@outlook.com>
2023-12-10 18:02:54 +0800

    smex for enhance minibuffer

--- init.el
+++ init.el
@@ -2,17 +2,40 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq inferior-ess-r-program "R")
 
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(setq tramp-ssh-controlmaster-options
-      "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
-
-(add-to-list 'load-path (locate-user-emacs-file "lisp"))
-(add-to-list 'load-path (locate-user-emacs-file "site-lisp"))
+;; --debug-init implies `debug-on-error'.
 (setq debug-on-error init-file-debug)
 
-;; (require 'init-ui)
+(require 'cl-lib)
+(defun add-subdirs-to-load-path (search-dir)
+  (interactive)
+  (let* ((dir (file-name-as-directory search-dir)))
+    (dolist (subdir
+             (cl-remove-if
+              #'(lambda (subdir)
+                  (or
+                   (not (file-directory-p (concat dir subdir)))
+                   (member subdir '("." ".."
+                                    "dist" "node_modules" "__pycache__"
+                                    "RCS" "CVS" "rcs" "cvs" ".git" ".github"))))
+              (directory-files dir)))
+      (let ((subdir-path (concat dir (file-name-as-directory subdir))))
+        (when (cl-some #'(lambda (subdir-file)
+                           (and (file-regular-p (concat subdir-path subdir-file))
+                                (member (file-name-extension subdir-file) '("el" "so" "dll"))))
+                       (directory-files subdir-path))
+          (add-to-list 'load-path subdir-path t))
+        (add-subdirs-to-load-path subdir-path)))))
+
+(let ((userdir (locate-user-emacs-file "lisp"))
+      (extdir  (locate-user-emacs-file "site-lisp")))
+  (add-subdirs-to-load-path (file-name-directory userdir))
+  (add-subdirs-to-load-path (file-name-directory extdir)))
+(setq custom-file (locate-user-emacs-file "custom.el"))
+
+
+(require 'init-ui)
+(require 'init-smex)
 ;; (require 'init-base)
 ;; (require 'init-funcs)
 ;; (require 'init-org)
