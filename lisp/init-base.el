47ca722e69c35a3c6dc01529c6e69d7230e8ab9e
haoran <haoran.mc@outlook.com>
2023-12-11 20:30:28 +0800

    ido + smex = basic minibuffer

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -3,17 +3,22 @@
 
 (set-language-environment "UTF-8")
 (global-auto-revert-mode t)      ;; load external changes automatically
+
 (setq make-backup-files nil)
 (setq create-lockfiles nil)
 (setq auto-save-default nil)
+
 (fset 'yes-or-no-p 'y-or-n-p)
+
 (setq dired-recursive-deletes 'always)         ;; recursive delete
 (setq dired-recursive-copies 'always)          ;; recursive copy
 (put 'dired-find-alternate-file 'disabled nil) ;; common buffer
+
 ;; work with upper sentence
 ;;(with-eval-after-load 'dired
 ;;   (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
 ;; (setq x-select-enable-clipboard t)             ;; shared with the system clipboard
+
 (save-place-mode t)                  ;; save cursor position
 (setq-default tab-width 4)           ;; tab length
 (setq-default indent-tabs-mode nil)  ;; use space rather tab
@@ -25,12 +30,13 @@
 ;; (setq abbrev-file-name "~/.emacs.d/bin/abbrev_defs")
 ;; (setq default-major-mode 'text-mode)
 
+
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 
 (setq scroll-margin 5 scroll-consrvatively 10000)
 
 
-
+(ido-mode)
 
 (require 'recentf)
 (recentf-mode 1)
